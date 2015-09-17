13431843426db46690b481ac2ea1efdb39512681
Jose Rodriguez <jmrbcu@gmail.com>
2015-09-16 23:15:55 -0500

    Added some comments

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -1,9 +1,12 @@
 # bash dot files
 unalias ls &> /dev/null
 mkdir ~/.old_bash_config
+
 git clone https://github.com/jmrbcu/dotfiles.git ~/.dotfiles
 for dotfile in `ls -A ~/.dotfiles/`; do
+  # check  if the file exists and if true, move it to the backup directory
   [ -L ~/$dotfile ] || [ -e ~/$dotfile  ] && mv $dotfile ~/.old_bash_config
+
   if [ $dotfile != ".git" ]; then
     ln -s ~/.dotfiles/$dotfile ~/$dotfile;
   fi
@@ -12,7 +15,9 @@ done
 # zsh configurations
 git clone --recursive https://github.com/jmrbcu/prezto.git ~/.zprezto
 for rcfile in `ls -A ~/.zprezto/runcoms/z*`; do
+  # check  if the file exists and if true, move it to the backup directory
   [ -L ~/.${rcfile##*/} ] || [ -e ~/.${rcfile##*/} ] && mv ~/.${rcfile##*/} ~/.old_bash_config
+
   ln -s $rcfile ~/.${rcfile##*/};
 done
 
@@ -20,6 +25,8 @@ done
 chsh -s /bin/zsh
 
 # vim configuration
+# check  if the file exists and if true, move it to the backup directory
 [ -L ~/.vim ] || [ -e ~/.vim ] && mv ~/.vim ~/.vim_old
+
 git clone https://github.com/timss/vimconf.git ~/.vim
 ln -sf ~/.vim/.vimrc ~/.vimrc
