e55c3e3ec80703dfa0dae4321a1692f884fb4b68
Jose Rodriguez <jmrbcu@gmail.com>
2015-09-16 23:12:21 -0500

    Fixed issue when the dot file exists and is a link

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -1,9 +1,9 @@
 # bash dot files
-unalias ls
+unalias ls &> /dev/null
 mkdir ~/.old_bash_config
 git clone https://github.com/jmrbcu/dotfiles.git ~/.dotfiles
 for dotfile in `ls -A ~/.dotfiles/`; do
-  [ -f ~/$dotfile ] && mv $dotfile ~/.old_bash_config
+  [ -L ~/$dotfile ] || [ -e ~/$dotfile  ] && mv $dotfile ~/.old_bash_config
   if [ $dotfile != ".git" ]; then
     ln -s ~/.dotfiles/$dotfile ~/$dotfile;
   fi
@@ -12,6 +12,7 @@ done
 # zsh configurations
 git clone --recursive https://github.com/jmrbcu/prezto.git ~/.zprezto
 for rcfile in `ls -A ~/.zprezto/runcoms/z*`; do
+  [ -L ~/.${rcfile##*/} ] || [ -e ~/.${rcfile##*/} ] && mv ~/.${rcfile##*/} ~/.old_bash_config
   ln -s $rcfile ~/.${rcfile##*/};
 done
 
@@ -19,6 +20,6 @@ done
 chsh -s /bin/zsh
 
 # vim configuration
-[ -f ~/.vim ] && mv ~/.vim ~/.vim_old
+[ -L ~/.vim ] || [ -e ~/.vim ] && mv ~/.vim ~/.vim_old
 git clone https://github.com/timss/vimconf.git ~/.vim
 ln -sf ~/.vim/.vimrc ~/.vimrc
