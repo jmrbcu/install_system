fb0b6758f9ff97e9ba967c4d41da3ed36b229caa
Jose Rodriguez <jrodriguez@interactivetel.com>
2015-10-06 22:12:01 -0500

    Moved Finder hack to the Finder category

--- scripts/basic.sh
+++ scripts/basic.sh
@@ -14,23 +14,6 @@ sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.serve
 echo -n  -e "\033[1;31mInstalling: \033[0;32mXCode Command Line Tools\033[0m"
 xcode-select --install
 
-# tell the Finder to show the folders first (HACK)
-if [ /System/Library/CoreServices/Finder.app/Contents/Resources/English.lproj ]; then
-  cd /System/Library/CoreServices/Finder.app/Contents/Resources/English.lproj
-  sudo mv /System/Library/CoreServices/Finder.app/Contents/Resources/English.lproj/InfoPlist.strings /System/Library/CoreServices/Finder.app/Contents/Resources/English.lproj/InfoPlist.strings.save
-  sudo plutil -replace Folder -string " Folder" /System/Library/CoreServices/Finder.app/Contents/Resources/English.lproj/InfoPlist.strings
-  sudo find / -name ".DS_Store"  -exec rm {} \;
-  defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-  defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
-  defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
-  defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
-  defaults write com.apple.finder NewWindowTarget PfHm
-  defaults write com.apple.finder FXPreferredViewStyle icnv
-  defaults write com.apple.finder FXPreferredGroupBy None
-  defaults write com.apple.finder FXArrangeGroupViewBy Name
-  sudo killall Finder
-fi
-
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write -g ApplePressAndHoldEnabled -bool false
 
@@ -152,6 +135,22 @@ defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
 defaults write com.apple.finder NewWindowTarget -string "PfLo"
 defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
 
+# tell the Finder to show the folders first (HACK)
+if [ /System/Library/CoreServices/Finder.app/Contents/Resources/English.lproj ]; then
+  cd /System/Library/CoreServices/Finder.app/Contents/Resources/English.lproj
+  sudo mv /System/Library/CoreServices/Finder.app/Contents/Resources/English.lproj/InfoPlist.strings /System/Library/CoreServices/Finder.app/Contents/Resources/English.lproj/InfoPlist.strings.save
+  sudo plutil -replace Folder -string " Folder" /System/Library/CoreServices/Finder.app/Contents/Resources/English.lproj/InfoPlist.strings
+  sudo find / -name ".DS_Store"  -exec rm {} \;
+  defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+  defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+  defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+  defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+  defaults write com.apple.finder NewWindowTarget PfHm
+  defaults write com.apple.finder FXPreferredViewStyle icnv
+  defaults write com.apple.finder FXPreferredGroupBy None
+  defaults write com.apple.finder FXArrangeGroupViewBy Name
+  sudo killall Finder
+fi
 
 ###############################################################################
 # Safari                                                                      #
